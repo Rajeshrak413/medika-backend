d22379be8c2eaca83b45e17c35fb47865c37239e
Rajeshrak413 <rajeshrak413@gmail.com>
2026-01-23 20:26:08 +0530

    Create package.js

---
+++ package.js
@@ -0,0 +1,18 @@
+{
+  "name": "medika-bazaar-backend",
+  "version": "1.0.0",
+  "description": "Logistics backend for Excel generation and Emailing",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js",
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "author": "Medika Bazaar",
+  "license": "ISC",
+  "dependencies": {
+    "cors": "^2.8.5",
+    "express": "^4.18.2",
+    "nodemailer": "^6.9.7",
+    "xlsx": "^0.18.5"
+  }
+}
