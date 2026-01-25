70af0db48bfec90554dde7257fa096df93af69af
Rajeshrak413 <rajeshrak413@gmail.com>
2026-01-25 06:19:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.post('/send-manifest', async (req, res) => {
         // 2. Prepare the Email Message
         const msg = {
             // Main Recipients
-            to: ['rajeshrak413@outlook.com','gokulkrishnan.velayutham@medikabazaar.com','bhaskar.r@medikabazaar.com','hanumanta.madival@medikabazaar.com], 
+            to: ['rajeshrak413@outlook.com','gokulkrishnan.velayutham@medikabazaar.com','bhaskar.r@medikabazaar.com','hanumanta.madival@medikabazaar.com'], 
             // CC Recipients (Add more here)
             cc: ['elumalai.b@medikabazaar.com'], 
             from: {
